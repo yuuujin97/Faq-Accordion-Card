f5caa63cd688ce1083dcedb6b7d374a102da7e52
Kim YuJin <yuuujin5468@gmail.com>
2021-09-10 14:47:29 +0900

    added click the button and image.

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const list = document.querySelector('.section__faq');
-console.log(list);
 
 list.addEventListener('click', function (event) {
   if (event.target.tagName === 'SPAN') {
     event.target.classList.toggle('show');
     event.target.nextElementSibling.classList.toggle('show');
+  } else if (event.target.tagName === 'BUTTON') {
+    event.target.parentNode.classList.toggle('show');
+    event.target.parentNode.nextElementSibling.classList.toggle('show');
+  } else if (event.target.tagName === 'IMG') {
+    event.target.parentNode.parentNode.classList.toggle('show');
+    event.target.parentNode.parentNode.nextElementSibling.classList.toggle('show');
   }
 });
